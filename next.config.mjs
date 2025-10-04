49274687e1eb79b1757ade4eb57b82a46be19922
Karthik <karthik@testpresss.in>
2025-10-04 18:57:47 +0530

    fix: Add support to deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import withMarkdoc from '@markdoc/next.js'
-
 import withSearch from './src/markdoc/search.mjs'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+  output: 'export', // ← static HTML export
+  trailingSlash: true,
+  basePath: '/trustcenter',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default withSearch(
